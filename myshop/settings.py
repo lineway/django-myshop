1d94bfa141468d137e8d7cea46e70a11a2575b85
zhangyiming <zhangyiming07@163.com>
2017-04-10 23:00:12 +0800

    配置mysql，增加urls.py在shop中

--- myshop/settings.py
+++ myshop/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop_db',
+        'USER': 'root',
+        'PASSWORD': '19910203',
+        'HOST': 'localhost',
     }
 }
 
